4e34a4d67b39e944072e635f7e66a48f4c69c347
Your Name <you@example.com>
2025-02-28 18:08:23 -0300

    python

---
+++ Operador1.py
@@ -0,0 +1,30 @@
+"""Write a code that initializes three variables, a, b and c 
+with the values 9, 2, and 11 (respectively).
+
+After that, initialize the following variables:
+
+d that will hold the result of a modulo 2 
+e that will hold the result of a modulo 3
+f that will hold the result of c modulo 10"""
+
+
+#Escribimos codigo a continuacion
+
+# Iniciamos variables
+a = 9
+b = 2
+c = 11
+
+# Perform modulo operation
+d = a % 2
+e = a % 3
+f = c % 10
+
+
+
+print ( f"a = {a}")
+print ( f"b = {b}")
+print ( f"c = {c}")
+print ( f"d = {d}")
+print ( f"e = {e}")
+print ( f"f = {f}")
